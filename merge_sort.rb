3231f1ba15d9adce01e197ab87c4d35c4d36bbd2
Dan Brubaker Horst <dbrubak1@nd.edu>
2023-01-26 19:22:24 -0500

    Initial success!

--- merge_sort.rb
+++ merge_sort.rb
@@ -3,10 +3,36 @@
 require "minitest/autorun"
 
 module MergeSort
+  def call(array)
+    puts "Input: #{array.inspect}"
+    length = array.length
+    return array if length <= 1
+
+    half = Integer(length / 2.0)
+    remainder = length % 2
+    puts "Length: #{length}"
+    puts "Half: #{half}"
+    puts "Remainder: #{remainder}"
+    #left_index = half + remainder
+    left_index = half - 1 + remainder
+    right_index = half
+
+    left = array[0..(left_index)]
+    puts "Left index: #{left_index}"
+    puts "Left: #{left}"
+    right = array[(right_index * -1), right_index]
+    puts "Right index: #{right_index}"
+    puts "Right: #{right}"
+
+    self.merge(self.call(left), self.call(right))
+  end
+
+  module_function :call
+
   def merge(left, right)
     sorted = []
     left_index, right_index = [0,0]
-    (1..(left.size + right.size)).each do |_pass|
+    (1..(left.size + right.size)).each do
       l = left[left_index]
       r = right[right_index]
 
@@ -21,6 +47,7 @@ module MergeSort
         right_index += 1
       end
     end
+    puts "Sorted: #{sorted}"
     sorted
   end
 
@@ -28,7 +55,22 @@ module MergeSort
 end
 
 class MergeSortTest < Minitest::Test
+  # The initial, proivded scinero doesn't require a full merge-sort as the
+  # provided arrays are each already sorted
   def test_sorted_arrays_of_equal_length
     assert_equal [1,2,3,4,5,6], MergeSort.merge([1,2,5],[3,4,6])
+  end
+
+  def test_sorted_arrays_of_unequal_length
+    assert_equal [1,2,3,4,5,6,7], MergeSort.merge([1,2,5,7],[3,4,6])
+  end
+
+  # It gets more complicated with unsorted input
+  def test_unsorted_array_of_even_length
+    assert_equal [1,2,3,4,5,6], MergeSort.call([5,1,2,4,3,6])
+  end
+
+  def test_unsorted_array_of_odd_length
+    assert_equal [1,2,3,4,5,6,7], MergeSort.call([5,1,2,7,4,3,6])
   end
 end
