8d83b162102f277e4bafdd3ed47979d624f667ec
Dan Brubaker Horst <dbrubak1@nd.edu>
2023-01-26 19:33:25 -0500

    Appeasing Rubocop

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,19 +1,19 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
-require "minitest/autorun"
+require 'minitest/autorun'
 
 module MergeSort
   def call(array)
-    puts "Input: #{array.inspect}"
     length = array.length
     return array if length <= 1
 
     half = Integer(length / 2.0)
     remainder = length % 2
 
-    self.merge(
-      self.call(array.slice(0, (half + remainder))),
-      self.call(array[(half * -1), half])
+    merge(
+      call(array.slice!(0, (half + remainder))),
+      call(array)
     )
   end
 
@@ -21,7 +21,8 @@ module MergeSort
 
   def merge(left, right)
     sorted = []
-    left_index, right_index = [0,0]
+    left_index = 0
+    right_index = 0
     (1..(left.size + right.size)).each do
       l = left[left_index]
       r = right[right_index]
@@ -37,7 +38,6 @@ module MergeSort
         right_index += 1
       end
     end
-    puts "Sorted: #{sorted}"
     sorted
   end
 
@@ -48,19 +48,19 @@ class MergeSortTest < Minitest::Test
   # The initial, proivded scinero doesn't require a full merge-sort as the
   # provided arrays are each already sorted
   def test_sorted_arrays_of_equal_length
-    assert_equal [1,2,3,4,5,6], MergeSort.merge([1,2,5],[3,4,6])
+    assert_equal [1, 2, 3, 4, 5, 6], MergeSort.merge([1, 2, 5], [3, 4, 6])
   end
 
   def test_sorted_arrays_of_unequal_length
-    assert_equal [1,2,3,4,5,6,7], MergeSort.merge([1,2,5,7],[3,4,6])
+    assert_equal [1, 2, 3, 4, 5, 6, 7], MergeSort.merge([1, 2, 5, 7], [3, 4, 6])
   end
 
   # It gets more complicated with unsorted input
   def test_unsorted_array_of_even_length
-    assert_equal [1,2,3,4,5,6], MergeSort.call([5,1,2,4,3,6])
+    assert_equal [1, 2, 3, 4, 5, 6], MergeSort.call([5, 1, 2, 4, 3, 6])
   end
 
   def test_unsorted_array_of_odd_length
-    assert_equal [1,2,3,4,5,6,7], MergeSort.call([5,1,2,7,4,3,6])
+    assert_equal [1, 2, 3, 4, 5, 6, 7], MergeSort.call([5, 1, 2, 7, 4, 3, 6])
   end
 end
